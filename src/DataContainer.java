af09b7067005cf37dcae499b8cb53d5bdb641ec6
AlexeyTen <aten9180929937@gmail.com>
2024-03-12 00:07:01 +0300

    first commit

---
+++ src/DataContainer.java
@@ -0,0 +1,69 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class DataContainer<T> {
+    private static final int capacity = 10;
+    private T[] data;
+    private int size = 0;
+
+    public DataContainer(Class<T> t) {
+        @SuppressWarnings("unchecked")
+        final T[] data = (T[]) Array.newInstance(t, capacity);
+        this.data = data;
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            int newCapacity = data.length * 2;
+            data = Arrays.copyOf(data, newCapacity);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Ошибка! Выход за пределы DataContainer");
+        }
+    }
+
+    public T get(int index) {
+        return data[index];
+    }
+
+    public void remove(int index) {
+        checkIndex(index);
+        for (int i = index + 1; i < data.length; i++) {
+            data[i - 1] = data[i];
+        }
+        data = Arrays.copyOf(data, data.length - 1);
+    }
+
+    public void add(T item) {
+        ensureCapacity();
+        data[size++] = item;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                i = data.length;
+            } else {
+                stringBuilder.append(data[i]).append(",");
+            } 
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = new Pair<>(10, "Hello");
+        System.out.println("First element: " + pair.getFirst());
+        System.out.println("Second element: " + pair.getSecond());
+
+        pair.setFirst(20);
+        pair.setSecond("World");
+        System.out.println("Updated first element: " + pair.getFirst());
+        System.out.println("Updated second element: " + pair.getSecond());
+
+
+
+        DataContainer<String> dataContainer = new DataContainer<String>(String.class);
+        dataContainer.add("a");
+        dataContainer.add("b");
+        dataContainer.add("s");
+        System.out.println(dataContainer);
+        dataContainer.remove(1);
+        System.out.println(dataContainer);
+        System.out.println(dataContainer.getSize());
+    }
+}
